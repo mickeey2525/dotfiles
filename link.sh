807b3f26bc0ecc30b27c157e3329393fa79dfc43
Mikio Tachibana <m.tachibana25@gmail.com>
2024-08-26 13:49:30 +0900

    add link.sh

---
+++ link.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+for dotfile in "${DOTFILES_DIR}"/.??* ; do
+    [[ "$dotfile" == "${DOTFILES_DIR}/.git" ]] && continue
+    [[ "$dotfile" == "${DOTFILES_DIR}/.github" ]] && continue
+    [[ "$dotfile" == "${DOTFILES_DIR}/.DS_Store" ]] && continue
+
+    ln -fnsv "$dotfile" "$HOME"
+done
